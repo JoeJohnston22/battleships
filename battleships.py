71e9c58521d6bb4a4cc384005919d55d61848e0e
JoeJohnston24 <joejohnston725@gmail.com>
2025-10-13 10:10:41 +0100

    Changed the grid creation and made it a const. made the grid axis easier to read. started to think about different ship sizes as well as placement.

--- battleships.py
+++ battleships.py
@@ -1,40 +1,47 @@
 import random
 
+GRID_SIZE = 10
+SHIPS = {"Cruiser": 2,"Destroyer": 3, "Battleship": 4,"Aircraft Carrier": 5}
+
 def create_grid():
-    width = int(input("Declare the width of the grid: "))
-    height = int(input("Declare the height of the grid: "))
-
-    opponent_game_grid = [['~' for _ in range(width)] for _ in range(height)]
-    opponent_display_grid = [['~' for _ in range(width)] for _ in range(height)]
-    player_display_grid = [['~' for _ in range(width)] for _ in range(height)]
+    opponent_game_grid = [['~'] * GRID_SIZE for _ in range(GRID_SIZE)]
+    opponent_display_grid = [['~'] * GRID_SIZE for _ in range(GRID_SIZE)]
+    player_display_grid = [['~'] * GRID_SIZE for _ in range(GRID_SIZE)]
     
     
-    return opponent_game_grid, opponent_display_grid, player_display_grid, width
+    return opponent_game_grid, opponent_display_grid, player_display_grid
 
-def display_player_grid(player_display_grid, width):
+def display_player_grid(player_display_grid):
     print()
     print("###############")
     print("Player 1")
     print("###############")
-    print(" " + " ".join(str(i) for i in range(width)))
     for row_num, row in enumerate(player_display_grid):
-        print(f"{row_num} " + " ".join(row))
+        display_num = GRID_SIZE - 1 - row_num
+        print(f"{display_num}" + " ".join(row))
+    
+    print(" " + " ".join(str(i) for i in range(GRID_SIZE)))
 
-def display_opponent_grid(opponent_display_grid, width):
+def display_opponent_grid(opponent_display_grid):
     print()
     print("###############")
     print("Player 2")
     print("###############")
-    print(" " + " ".join(str(i) for i in range(width)))
     for row_num, row in enumerate(opponent_display_grid):
-        print(f"{row_num} " + " ".join(row))
-
-def create_opponent_ship(opponent_game_grid, width):
+        display_num = GRID_SIZE - 1 - row_num
+        print(f"{display_num}" + " ".join(row))
+ 
+    print(" " + " ".join(str(i) for i in range(GRID_SIZE)))
+    
+# create_aircraft_carrier():       
+# create_battleship():
+# create_destroyer():
+# create_cruiser():
+    
+def create_opponent_ship(opponent_game_grid):
     num_ship = 4
     ship = 's'
     
-    print(" " + " ".join(str(i) for i in range(width)))
-
     for _ in range(num_ship):
         while True:
             y = random.randint(0, len(opponent_game_grid[0]) - 1)
@@ -43,7 +50,8 @@ def create_opponent_ship(opponent_game_grid, width):
             if opponent_game_grid[y][x] == '~':
                 opponent_game_grid[y][x] = ship
                 break
-
+    # print(opponent_game_grid)
+    
 def player_offence(opponent_game_grid, opponent_display_grid):
     num_ships = 4
     turn_number = 1
@@ -57,7 +65,8 @@ def player_offence(opponent_game_grid, opponent_display_grid):
         y_coord = input("Enter the y coords: ")
         print("###############")
         x = int(x_coord)
-        y = int(y_coord)
+        y_input = int(y_coord)
+        y = GRID_SIZE - 1 - y_input
         if (0 <= x < len(opponent_game_grid) and 0 <= y < len(opponent_game_grid[0])):  
         
             if (opponent_game_grid[y][x] == 's'):
@@ -80,17 +89,20 @@ def player_offence(opponent_game_grid, opponent_display_grid):
                 print("###############")
                 print() 
                 turn_number += 1 
-                
-        print("\n   " + " ".join(str(i) for i in range(len(opponent_display_grid[0]))))  
+                  
         for row_num, row in enumerate(opponent_display_grid):
-            print(f"{row_num}  " + " ".join(row))  
+            display_num = GRID_SIZE - 1 - row_num
+            print(f"{display_num}" + " ".join(row))
+        
+        print(" " + " ".join(str(i) for i in range(len(opponent_display_grid[0]))))
+        
     print("YOU WIN, ALL OF THE ENEMY HAS BEEN ELIMINATED.")
 
 
 if __name__ == "__main__":
     print("Battleships")
-    opponent_game_grid, opponent_display_grid, player_display_grid, width = create_grid()
-    display_player_grid(player_display_grid, width)
-    display_opponent_grid(opponent_display_grid, width)
-    create_opponent_ship(opponent_game_grid, width)
+    opponent_game_grid, opponent_display_grid, player_display_grid = create_grid()
+    display_player_grid(player_display_grid)
+    display_opponent_grid(opponent_display_grid)
+    create_opponent_ship(opponent_game_grid)
     player_offence(opponent_game_grid, opponent_display_grid)
